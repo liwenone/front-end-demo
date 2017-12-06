76f35b31ab8844373fda28618f91971150ee9339
lwvoid <lwvoid@163.com>
2017-12-07 01:45:26 +0800

    IOS test

--- 20171206-test/js/index.js
+++ 20171206-test/js/index.js
@@ -1,16 +1,26 @@
 //解决第三方软键盘唤起时底部input输入框被遮挡问题
+var is = false;
 var bfscrolltop = document.body.scrollTop; //获取软键盘唤起前浏览器滚动部分的高度
 $("input.inputframe").focus(function() { //在这里‘input.inputframe’是我的底部输入栏的输入框，当它获取焦点时触发事件
     resize();
+    is = true;
 }).blur(function() { //设定输入框失去焦点时的事件
     resize();
+    is = false;
 });
 
 function resize() {
     setTimeout(function() {
-        var height = $(window).height() - 100 - $(window).scrollTop();
+        var height = $(window).height() - $(window).scrollTop();
         $('.content').height(height);
     }, 2000);
 }
+
+$(window).on('scroll', function(e) {
+    if (is) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+})
 
 resize();
